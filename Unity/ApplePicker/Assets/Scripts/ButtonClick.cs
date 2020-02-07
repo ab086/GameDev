69dee8a7e07cb03ea2c5bb7bd0970e4b20797495
Austin Blair <ab0860229@otc.edu>
2020-02-07 00:56:47 -0600

    Finished Unity ver.; Unreal ver. in dev

---
+++ Unity/ApplePicker/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour {
+
+	// Update is called once per frame
+	public void PlayGame(string sceneLoad) {
+        SceneManager.LoadScene(sceneLoad);
+	}
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit(); // Does not work in Unity editor
+    }
+}
